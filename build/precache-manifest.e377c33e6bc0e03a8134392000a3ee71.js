24d58da0e317ddd3327725a6b42ca216e990974d
CakeCrusher <37946988+CakeCrusher@users.noreply.github.com>
2019-10-21 12:10:41 -0400

    second_commit

---
+++ build/precache-manifest.e377c33e6bc0e03a8134392000a3ee71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83643ed1c44ac364781aeb0c0b3becf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebcd3eda4c3849b235a9",
+    "url": "/static/css/main.cfab1171.chunk.css"
+  },
+  {
+    "revision": "9d7d7637ffdb3543cf9b",
+    "url": "/static/js/2.bf711040.chunk.js"
+  },
+  {
+    "revision": "ebcd3eda4c3849b235a9",
+    "url": "/static/js/main.31201dce.chunk.js"
+  },
+  {
+    "revision": "17d0ae5722a7a1d90858",
+    "url": "/static/js/runtime-main.86de38e2.js"
+  }
+]);
